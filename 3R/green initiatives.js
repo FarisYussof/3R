9b95090c0a6ef5fb8087426c671a1f1644cd7da8
FarisYussof <48983438+FarisYussof@users.noreply.github.com>
2019-03-28 01:55:11 +0800

    Add files via upload

---
+++ 3R/green initiatives.js
@@ -0,0 +1,8 @@
+/*var button=document.querySelector('button');
+
+
+function change(){
+  var image1=document.getElementById('button1').src="3R/plastic usage.jpg";
+}
+
+button.addEventListener('click',change);
